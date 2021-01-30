e43c5efa7fca114e1276d9cec604e9403aa1f26f
Rafa <rpadiper@gmail.com>
2021-01-29 23:14:58 -0600

    all animations work great :D

--- GGJ/Assets/Scripts/PlayerController.cs
+++ GGJ/Assets/Scripts/PlayerController.cs
@@ -5,39 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    //private variables
+   //private variables
     private Rigidbody2D rb;
     private float inputX;
 
     //public variables
     public float moveSpeed, jumpspeed;
-    private bool isGrounded;
+    private bool isGrounded, canJump, jumpAnim;
     public Animator anim;
+    public SpriteRenderer rend;
      
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        inputX = Input.GetAxisRaw("Horizontal");
-       
-        if (inputX != 0)
-        {
-            rb.velocity = new Vector2( inputX * moveSpeed, rb.velocity.y);
-        }
-
-        if (isGrounded && Input.GetButton("Jump"))
-        {
-            rb.AddForce(Vector2.up * jumpspeed);
-        }
-        
-
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "floor") {
+        if (collision.gameObject.tag == "floor")
+        {
             isGrounded = true;
         }
     }
@@ -48,4 +34,66 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
     }
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+
+        //check if we are grounded to jump in fixedupdate
+        if (isGrounded)
+        {
+            jumpAnim = true;
+
+            if (Input.GetButton("Jump")) {
+                canJump = true;
+            }
+            
+        }
+        else {
+            jumpAnim = false;
+            canJump = false;
+        }
+
+        //check if we move and if so, move in the wasd direction
+        if (inputX != 0 && jumpAnim == true)
+        {
+            anim.SetBool("IsJumping", false);
+            anim.SetBool("IsIdle", false);
+            anim.SetBool("IsRunning", true);
+            rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
+        }
+        else if (inputX != 0 && jumpAnim == false)
+        {
+            anim.SetBool("IsJumping", true);
+            anim.SetBool("IsRunning", false);
+            anim.SetBool("IsIdle", false);
+            rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
+        }
+        else {
+            anim.SetBool("IsIdle", true);
+            anim.SetBool("IsRunning", false);
+        }
+
+        //make the sprite face the direction we move by flipping it
+        if (inputX > 0)
+        {
+            rend.flipX = false;
+        }
+        else {
+            rend.flipX = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        //animations and jumping
+        if (canJump == true)
+        {
+            anim.SetBool("IsJumping", true);
+            rb.AddForce(Vector2.up * jumpspeed);
+        }
+        else {
+            anim.SetBool("IsIdle", true);
+            anim.SetBool("IsJumping", false);
+        }
+    }
 }
+
