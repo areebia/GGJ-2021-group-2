d0521e73a2e5a5527a4728a51c484675c1f14d15
Rafa <rpadiper@gmail.com>
2021-01-30 18:05:12 -0600

    droplet

---
+++ GGJ/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public static float health;
+
+    void Start()
+    {
+        health = 1f;
+    }
+    void Update()
+    {
+        Debug.Log("Health is " + health);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "drop" && health < 2) {
+            health += .1f;            
+            transform.localScale = new Vector2(transform.localScale.x + .1f, transform.localScale.y + .1f);
+            collision.gameObject.GetComponent<ParticleSystem>().Play();
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
