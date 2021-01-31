74de16f993d9f4638ef4cf045b22bc92a7ff570a
Rafa <rpadiper@gmail.com>
2021-01-31 12:50:30 -0600

    healthbar works

--- GGJ/Assets/Scripts/HealthBar.cs
+++ GGJ/Assets/Scripts/HealthBar.cs
@@ -6,18 +6,18 @@ public class HealthBar : MonoBehaviour
 {
     private float health;
     private SpriteRenderer rend;
-    private Object[] sprites;
+    public Object[] sprites;
     private void Start()
     {
         rend = GetComponent<SpriteRenderer>();
         sprites = Resources.LoadAll("healthbar");
     }
-    
+
     void Update()
     {
         health = PlayerHealth.health;
         int i = (int)Mathf.Round((health - 1) * 10);
-        Debug.Log("current sprite is " +  i);
+        Debug.Log("current sprite is " + i);
         rend.sprite = (Sprite)sprites[i + 1];
     }
 }
