83f6d5d8fda037b73267c88ad2ffdee80712e212
Areeb Yaqub <aygohan@gmail.com>
2021-02-01 01:40:41 -0800

    updates to win scene

--- GGJ/Assets/button.cs
+++ GGJ/Assets/button.cs
@@ -17,28 +17,29 @@ public class button : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-
-    /*public void QuitGame()
+    /*
+    public void QuitGame()
     {
         // save any game data here
-    #if UNITY_EDITOR
+#if UNITY_EDITOR
         // Application.Quit() does not work in the editor so
         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
         UnityEditor.EditorApplication.isPlaying = false;
-    #else
+#else
          Application.Quit();
-    #endif
-    }*/
-
+#endif
+    }
+    */
     // Start is called before the first frame update
     void Start()
     {
-        
+         Cursor.visible = true;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
